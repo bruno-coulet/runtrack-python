6d98fb953d65ef975cc42b7166c0da72fa37bba9
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-20 12:12:26 +0100

    job07

---
+++ jour01/job07/calcul.py
@@ -0,0 +1,10 @@
+# Créez un programme qui affiche la somme des variables “num1” et “num2” dans le
+# terminal. Assignez la valeur de “40” à “num1” et la valeur “2” à “num2”.
+
+
+num1=40             #   crée la variable num1 avec la valeur 40
+num2=2              #   crée la variable num2 avec la valeur 2
+
+calcul=(num1+num2)  #   la variable calcul stocke somme des variables num1 et num2
+
+print(calcul)       #   affiche la variable calcul
