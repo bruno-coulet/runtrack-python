48ee6c7226332af1066ef9e9a28d73dde4a3d2c1
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-20 11:35:34 +0100

    job04

---
+++ jour01/job04/main.py
@@ -0,0 +1,13 @@
+# Première ébauche
+#  for i in(alphabet déjà stocké quelque part)
+#     print(i)
+#     i=i+1
+
+# Chat GPT
+# for i in range(ord('A'), ord('Z')+1):
+#     print(chr(i), end=' ')
+# print("\n")
+
+# Recherche gogol : pyhton module alphabet > https://www.delftstack.com/
+import string
+print(string.ascii_lowercase)
