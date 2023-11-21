f72cbaa942b85cd44b7d8b83cc5f2334744e7784
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-21 10:32:11 +0100

    Ajout de jour02 job01

---
+++ jour02/job01.py
@@ -0,0 +1,8 @@
+# Créez un programme qui parcourt les nombres de 0 à 20, affichez chacun des chiffres
+# dans le terminal.
+
+i=0             #   initialisation
+
+while i <= 20:  #   condition
+    print(i)    #   instructions
+    i+=1        #   incrémentation
