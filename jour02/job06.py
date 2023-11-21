b4972f4dc33601530f306265b0c439dbcb23db80
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-21 14:52:30 +0100

    correction job06

--- jour02/job06.py
+++ jour02/job06.py
@@ -1,24 +1,12 @@
 # Écrire un programme qui affiche les nombres premiers jusqu’à 1000.
 
-
-#   initialise i
-i=1                                        
-#   La variable a stocke i divisé par 1         -   doit retourner un entier
-a = i/1
-#   variable b stocke i divisé par lui même     -   doit retourner 1
-b = i/i
-
-
-
-#i%2!=0                                     #   les nombre entiers sont tous impairs sauf 2
-#while a.is_integer():  
- #  tant que la division par 1 retourne un entier     
-    
-while  i <= 1000:   #  tant que i=2 ou que i est impair et i <= 1000
-    i+=1             
-    while i == 2 or i % 2 != 0:
-        print(i)                                 #   affiche i
-                                       #   incremente i
+for n in range(1,1000 + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
     
 
 
