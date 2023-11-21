6e6a01bed4e865a2eb777c17ae4e174ca3c19aa2
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-21 10:36:01 +0100

    ajout de job02 qui n'affiche pas 20

---
+++ jour02/job02.py
@@ -0,0 +1,10 @@
+# Créer un programme qui parcourt les nombres de 0 à 20, affichez 1
+# nombre sur 2 dans le terminal.
+
+
+
+i=0             #   initialisation
+
+while i < 20:   #   condition modifié par rapport à l'exercice précédent pour ne pas afficher 20
+    print(i)    #   instructions
+    i+=2        #   incrémentation
