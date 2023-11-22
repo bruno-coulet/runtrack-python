f72e394fc482d2e1946583708bea2b751e99e6b0
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-22 10:55:43 +0100

    Add job03

---
+++ jour03/job03/job03.py
@@ -0,0 +1,14 @@
+# Créer une fonction nommée “Add”. Cette fonction devra prendre deux nombres entiers
+# en paramètres et afficher la somme de ces deux entiers.
+# Depuis votre programme, appelez cette fonction plusieurs fois en y passant des
+# paramètres différents pour afficher ces résultats.
+
+
+def Add(a,b):
+    a=int(a)
+    b=int(b)
+    print(a+b)
+
+Add(1,1)
+Add(2,2)
+Add(3,3)
