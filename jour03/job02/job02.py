88561ecb5873a301caed8b2e4d2077a2063c9899
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-22 10:45:54 +0100

    Add job01 and job02 folders

---
+++ jour03/job02/job02.py
@@ -0,0 +1,12 @@
+# Créer une fonction nommée My_print_name. Cette fonction prendra un variable nommé
+# “name” en paramètre. Elle affichera la valeur de name. Depuis votre programme,
+# appelez cette fonction plusieurs fois en y passant des paramètres différents pour
+# afficher ces résultats.
+
+def My_print_name(name):
+    print(name)
+
+    
+My_print_name ("riri")
+My_print_name ("fifi")
+My_print_name ("loulou")
