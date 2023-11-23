ad04361e445598ae01e5f7fb600309d4d2430d62
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-23 10:44:07 +0100

    job03.py

---
+++ jour04/job03/job03.py
@@ -0,0 +1,10 @@
+# Écrire une fonction qui contient une liste nommée “fruits” qui contient les strings
+# “pomme”, “cerise”, “orange”. 
+
+def verger():
+    fruits = ["pomme", "cerise", "orange"]
+    fruits.append("Melon")      #   Cette fonction doit à son appel ajouter à la liste “fruits” le string “Melon” à la fin de cette liste.
+    return fruits
+
+
+print(verger())     # == print (fruits)  donc cela affiche la liste
