82372c7d21ebf6a2e2ca967b9f638d1997743555
bruno-coulet <bruno.coulet@laplateforme.io>
2023-11-27 17:42:44 +0100

    job06

---
+++ jour05/job06/job06.py
@@ -0,0 +1,11 @@
+
+
+def phare(marches,cm):
+
+    hauteur =marches*cm/100
+    metres=hauteur*70
+
+
+    print(f" Pour {marches} marches de {cm} cm, le gardien parcourt {metres} m par semaine.")
+
+phare(100,10)
